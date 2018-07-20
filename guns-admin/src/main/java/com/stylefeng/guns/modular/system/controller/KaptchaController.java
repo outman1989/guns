4df3074aa82600d3a596c2f04d321ff39369c871
刘新 <454958231@qq.com>
2018-07-20 15:46:37 +0800

    1、代码修改

--- guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/KaptchaController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/KaptchaController.java
@@ -2,6 +2,7 @@ package com.stylefeng.guns.modular.system.controller;
 
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.stylefeng.guns.config.properties.GunsProperties;
 import com.stylefeng.guns.core.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,10 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * 验证码生成
@@ -29,14 +32,64 @@ public class KaptchaController {
 
     @Autowired
     private GunsProperties gunsProperties;
-
-    @Autowired
-    private Producer producer;
+//    @Autowired
+//    private Producer producer;
 
     /**
-     * 生成验证码
+     * 获取图片验证码
+     * 本地接口地址：http://127.0.0.1:8660/api/trade/web/validate/getCode.do
      */
     @RequestMapping("")
+    public void getCode(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        int width = 60, height = 20;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        Random random = new Random();
+        g.setColor(getRandColor(200, 250));
+        g.fillRect(0, 0, width, height);
+        g.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        g.setColor(getRandColor(160, 200));
+        for (int i = 0; i < 155; i++) {
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            int xl = random.nextInt(12);
+            int yl = random.nextInt(12);
+            g.drawLine(x, y, x + xl, y + yl);
+        }
+        StringBuilder sRand = new StringBuilder("");
+        for (int i = 0; i < 4; i++) {
+            String rand = String.valueOf(random.nextInt(10));
+            sRand.append(rand);
+            g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
+            g.drawString(rand, 13 * i + 6, 16);
+        }
+        // 将认证码存入SESSION
+        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_KEY, sRand.toString());
+        g.dispose();
+        try {
+            ImageIO.write(image, "JPEG", response.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Color getRandColor(int fc, int bc) {
+        Random random = new Random();
+        if (fc > 255) fc = 255;
+        if (bc > 255) bc = 255;
+        int r = fc + random.nextInt(bc - fc);
+        int g = fc + random.nextInt(bc - fc);
+        int b = fc + random.nextInt(bc - fc);
+        return new Color(r, g, b);
+    }
+
+
+    /**
+     * 生成验证码（不用）
+     */
     public void index(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
 
@@ -55,6 +108,7 @@ public class KaptchaController {
         response.setContentType("image/jpeg");
 
         // create the text for the image
+        Producer producer = new DefaultKaptcha();
         String capText = producer.createText();
 
         // store the text in the session
